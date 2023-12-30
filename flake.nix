2bc2b92f9a8958edfaea4b311e4a8a6567bdc6ba
John Murray <5672686+JRMurr@users.noreply.github.com>
2023-12-29 19:30:52 -0500

    add nix flake for development

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    roc.url = "github:roc-lang/roc";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, roc, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+
+        rocPkgs = roc.packages.${system};
+        rocFull = rocPkgs.full;
+
+        rustVersion = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml);
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustVersion;
+          rustc = rustVersion;
+        };
+
+        rust-shell =
+          (rustVersion.override { extensions = [ "rust-src" "rust-analyzer" ]; });
+
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs;
+              [
+                rocFull
+                rust-shell
+              ];
+
+            shellHook = ''
+              export ROC_LSP_PATH=${rocFull}/bin/roc_ls
+
+            '';
+          };
+        };
+      });
+}
